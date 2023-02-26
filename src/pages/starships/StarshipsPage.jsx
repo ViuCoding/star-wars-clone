fba92a481a3b9bfa720a0b66e8731abe65370d6c
Vincenzo- <viulenzgg@gmail.com>
2023-02-26 10:54:55 +0100

    added new images, tweaked media queries for 4 components

--- src/pages/starships/StarshipsPage.jsx
+++ src/pages/starships/StarshipsPage.jsx
@@ -2,13 +2,13 @@
 import Hero from "../../components/Hero";
 import StarshipsList from "../../components/StarshipsList";
 
-import HeroBg1 from "../../assets/bg1.jpg";
+import HeroBg6 from "../../assets/bg6.jpg";
 
 export default function StarshipsPage() {
   return (
     <div className='starships-page container'>
       <Hero
-        src={HeroBg1}
+        src={HeroBg6}
         message='“I’m a Mandalorian. Weapons are part of my religion.”'
         text='Mando, The Mandalorian'
       />
