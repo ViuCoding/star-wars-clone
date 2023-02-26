40337bcd38635a500e62ead8faadc86a3800fcdc
Vincenzo- <viulenzgg@gmail.com>
2023-02-26 14:24:43 +0100

    added route guard to starships page

--- src/pages/starships/StarshipsPage.jsx
+++ src/pages/starships/StarshipsPage.jsx
@@ -1,21 +1,37 @@
+// styles
+import "./StarshipsPage.scss";
+
 // components
 import Hero from "../../components/Hero";
 import StarshipsList from "../../components/StarshipsList";
 
 // images
+import LogoYellow from "../../assets/SWlogoYellow.png";
 import HeroBg6 from "../../assets/bg6.jpg";
 
 // context
 import { useUserContext } from "../../hooks/useUserContext";
+import { Link } from "react-router-dom";
 
 export default function StarshipsPage() {
   const { user } = useUserContext();
 
   if (!user) {
     return (
-      <div className='container'>
+      <div className='no-user container'>
         {" "}
-        <h1>YOU MUST LOG IN FIRST</h1>
+        <h1>PLEASE LOG IN, YOUNG PADAWAN</h1>
+        <div className='image-container'>
+          <img src={LogoYellow} alt='' />
+        </div>
+        <div className='redirects'>
+          <Link to='/login' className='user-btn'>
+            LOGIN
+          </Link>
+          <Link to='/signup' className='user-btn'>
+            SIGN UP
+          </Link>
+        </div>
       </div>
     );
   }
